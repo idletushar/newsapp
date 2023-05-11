ae0271ba54940331f17a2134fff77c8483742164
Tushar <tushar8650@outlook.com>
2023-05-11 13:06:16 +0530

    added api and componentDidmount()

--- src/Comps/NewsItem.js
+++ src/Comps/NewsItem.js
@@ -11,10 +11,11 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+          {/* ternary operator */}
+        <img src={!imageUrl?"https://propertywiselaunceston.com.au/wp-content/themes/property-wise/images/no-image.png":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <h5 className="card-title">{title}...</h5>
+        <p className="card-text">{description}...</p>
         <a href={newsUrl} target='_blank' className="btn btn-primary">Read More</a>
         </div>
         </div>
